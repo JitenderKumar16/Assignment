187998c56f3c39df3d337f0b335fd355f92a91fa
JitenderKumar16 <129144970+JitenderKumar16@users.noreply.github.com>
2023-03-28 14:40:31 +0530

    Add files via upload

---
+++ Division Problem.py
@@ -0,0 +1,14 @@
+# Implement division of two positive integers without using the division, multiplication, or modulus operators. Return the quotient as an integer, ignoring the remainder
+
+def divide(dividend, divisor):
+    quotient = 0
+    while dividend >= divisor:
+        dividend -= divisor
+        quotient += 1
+    return quotient
+
+a = float(input("Enter the value of Dividend: "))
+b = float(input("Enter the value of Divisor: "))
+
+print("Result:", divide(a,b))
+
